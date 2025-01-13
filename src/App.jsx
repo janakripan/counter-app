8e79ab9a2b3a4d65a449438d8731798814d6c5ce
janakripan <janakripanjkc@gmail.com>
2025-01-13 13:32:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useReducer } from 'react'
+
+
+
+const reducer = (state, action) =>{
+  
+  switch (action.type){
+    
+    
+    case 'increment':{
+      const newCount = state.count + 1
+      const hasError = newCount>5
+      return{...state ,
+         count:hasError? state.count: newCount,
+         error: hasError? "Maximum reached" : null,
+
+       }
+    }
+    case 'decrement':{
+      const newCount = state.count - 1
+      const hasError = newCount<0
+      return{...state ,
+         count:hasError? state.count: newCount,
+         error: hasError? "Minimum reached" : null,
+
+       }
+    }
+    default:
+      return state
+  }
+  
+  
+
+}
+
+
+
+function App() {
+  const [state, dispatch] = useReducer(reducer , {
+    count: 0 ,
+    error : null ,
+
+  })
+  console.log(state.count)
+
+  return (
+    <>
+     <div className='w-full h-screen flex items-center justify-center'>
+
+      <div className='w-fit h-fit flex flex-col items-center justify-center'>
+
+        <div className='w-16 h-8 flex items-center justify-center rounded-lg border-2 mb-8'>{state.count}</div>
+        
+        {state.error&&(<div className="w-fit mb-3 text-red-700">{state.error}</div>)}
+
+        <div className='flex flex-row gap-x-2 '>
+          <button className='px-5 py-2 bg-blue-400 rounded-lg text-white'
+                onClick={()=>dispatch({type : 'decrement'} )}
+          > decrement</button>
+          <button className='px-5 py-2 bg-blue-400 rounded-lg text-white'
+                onClick={()=>dispatch({type : 'increment'}) }
+          > increment</button>
+        </div>
+        
+      </div>
+
+     </div>
+    </>
+  )
+}
+
+export default App
